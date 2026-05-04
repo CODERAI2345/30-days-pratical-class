6ce301ef71351d0c917a741d6d777db3ad4af585
Kailash <kailashee042@gmail.com>
2026-05-04 21:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+print(" GitHub Actions Pipeline Running Successfully!")
+
+name = "Kailash"
+print(f"Hello {name}, your CI/CD pipeline is working!")
+
+# simple logic
+a = 5
+b = 10
+print("Sum:", a + b)
